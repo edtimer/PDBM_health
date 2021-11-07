46dbc0106021edf2880b1fa9079f362f80e1509f
edtimer <26600954+edtimer@users.noreply.github.com>
2021-11-07 14:01:48 +0000

    added draft management page and factored the authentication servlet

--- PDBM/src/main/java/com/Authenticate/Authentication.java
+++ PDBM/src/main/java/com/Authenticate/Authentication.java
@@ -3,7 +3,8 @@ package com.Authenticate;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
-import java.security.DrbgParameters.Reseed;
+import java.lang.WeakPairMap.Pair.Weak;
+import java.time.temporal.TemporalAdjuster;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,26 +15,32 @@ import javax.swing.text.View;
 
 import org.apache.catalina.connector.CoyoteWriter;
 import org.apache.jasper.tagplugins.jstl.core.Out;
+import org.eclipse.jdt.internal.compiler.classfmt.AnnotationMethodInfoWithAnnotations;
 
 @SuppressWarnings("serial")
 public class Authentication extends HttpServlet {
 
 	protected void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 
-		PrintWriter writer = res.getWriter();
+//		PrintWriter writer = res.getWriter();
 		String emailString = req.getParameter("email");
 		String password = req.getParameter("pass");
-//we need to use equals because String is not a primative data type in java
+//we need to use equals because String is not a primitive data type in java
 		if (emailString.equals("test@test.com") && password.equals("pass")) {
 
 			RequestDispatcher view = req.getRequestDispatcher("main_Page.jsp");
-//todo:
+//TODO:
 //			writer.print(view);
 			view.forward(req, res);
 //			writer.print("okay");
 		} else {
-			RequestDispatcher nologin_view = req.getRequestDispatcher("Login_page.jsp");
-			nologin_view.forward(req, res);
+
+			res.sendRedirect("Login_page.jsp");		
+			
+//			if Weak need to redirect with the whole session
+//			RequestDispatcher nologin_view = req.getRequestDispatcher("Login_page.jsp");
+//			nologin_view.forward(req, res);
+			
 		}
 	}
 }
