b446dcb2c49d0b62b79d639c760ce285b53caa1e
edtimer <26600954+edtimer@users.noreply.github.com>
2021-11-15 16:35:26 +0000

    added attributes to user class

--- PDBM/src/main/java/com/Authenticate/User.java
+++ PDBM/src/main/java/com/Authenticate/User.java
@@ -5,6 +5,20 @@ import java.util.UUID;
 public class User {
 
 	private UUID uid;
+
+	/**
+	 * @return the uid
+	 */
+	public UUID getUid() {
+		return uid;
+	}
+
+	/**
+	 * @param uid the uid to set
+	 */
+	public void setUid(UUID uid) {
+		this.uid = uid;
+	}
 	
 	
 }
